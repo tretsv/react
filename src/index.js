cd3225d772929989b17224d03f01ae1be2d941f1
Sergey Tretyakov <stretyakov@mackiev.com>
2020-03-12 15:52:50 +0200

    2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "./index.css";
 
 const data = {
 	title: "some title",
@@ -9,7 +10,7 @@ const data = {
 };
 
 function Image(props) {
-	return <img src={props.src} alt={props.alt} />;
+	return <img src={props.src} alt={props.alt} style={{width: "100%"}}/>;
 }
 
 class MovieItem extends React.Component {
@@ -17,22 +18,41 @@ class MovieItem extends React.Component {
 		super();
 		this.state = {
 			show: true,
+			liked: false
 		}
 	}
+
+	toggleRate = () => {
+		this.setState({show: !this.state.show});
+	};
+
+	handleLike = () => {
+		this.setState({liked: !this.state.liked});
+	};
 
 	render() {
 		const {data: {title, image, rate, overview}} = this.props;
 		console.log("state: ", this.state);
 		return (
-			<div>
+			<div style={{width: "300px"}}>
 				<Image src={image} alt={title} />
 				<p>{title}</p>
 				<p>{rate}</p>
-				<button type="button" onClick={() => {
-					this.setState({show: !this.state.show});
-				}}>
-					{this.state.show ? "hide" : "show"}
-				</button>
+				<div style={{display: "flex", justifyContent: "space-between"}}>
+					<button
+						type="button"
+						onClick={this.toggleRate}
+					>
+						{this.state.show ? "hide" : "show"}
+					</button>
+					<button
+						type="button"
+						onClick={this.handleLike}
+						className={this.state.liked ? "btn--liked" : ""}
+					>
+						Like
+					</button>
+				</div>
 				{this.state.show ? <p>{overview}</p> : null}
 			</div>
 		);
